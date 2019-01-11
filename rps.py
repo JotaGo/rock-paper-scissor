ee3eedf6f40a19d6a70847a14c4c3dce59f5a54a
Juan Márquez <jemarqueza1993@gmail.com>
2019-01-10 22:43:23 -0400

    writing logic

--- rps.py
+++ rps.py
@@ -1,2 +1,27 @@
+
+import random
+
+#Global variable
+option = ['rock','paper','scissor']
+
 if __name__ == "__main__":
-    pass
+    game_on = True
+
+    while game_on:
+        print('Choose one of the three options:')
+        c = input('''
+            [1] - Rock
+            [2] - Paper
+            [3] - Scissor
+        \n''')
+        cpu_option = option[random.randint(0,2)]
+        print(cpu_option)
+        if c == 'y':
+            game_on = True
+        elif c == 'n':
+            game_on = False
+        else:
+            print('invalid input')
+    
+    print('bye bye')
+        
