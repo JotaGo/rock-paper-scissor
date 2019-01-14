575677ad8d65a70fd535dfddf9fa97c8a4ca8e5c
Juan Márquez <jemarqueza1993@gmail.com>
2019-01-13 23:43:02 -0400

    adding exception control

--- rps.py
+++ rps.py
@@ -1,21 +1,49 @@
 
 import random
 
-#Global variable
+# Global variable
 option = ['rock','paper','scissor']
 
+# Function to let user enter the option
+# 1 is Rock
+# 2 is Paper
+# 3 is Scissor
+# if the user Try to enter a string in the input, the program will say "Data entered not valid, please enter a valid option"
+# and if the user enter a number higher than 3 or lower than 1, the program will print "Enter a number between 1 and 3"
+# then it will return to this function 
+def data_input():
+    while True:
+        try:
+            player = int(input('''
+                [1] - Rock
+                [2] - Paper
+                [3] - Scissor
+            \n'''))
+            return player
+        except ValueError:
+            print('Data entered not valid, please enter a valid option')
+
+# this function is to avoid repeating the same print
+def result(win,cpu_option):
+    if win:
+        print('You win! The CPU choose', cpu_option)
+    else:
+        print('You lose! The CPU choose', cpu_option)
+
+
+# Main function
 if __name__ == "__main__":
     game_on = True
     cnt = [0,0]
+    player = 0
     while game_on:
         print('Choose one of the three options:')
-        player = int(input('''
-            [1] - Rock
-            [2] - Paper
-            [3] - Scissor
-        \n'''))
-#        if player != 1 or player != 2 or player != 3:
-#            print('Please insert a valid option')
+
+        while player > 3 or player <1:
+            player = data_input()
+            if player > 3 or player <1:
+                print('Enter a number between 1 and 3')
+            
         if player <= 3 and player > 0:
             player -= 1
         player_option = option[player]
@@ -25,23 +53,30 @@ if __name__ == "__main__":
             print('Draw')
         elif player_option == 'rock':
             if cpu_option == 'scissor':
-                print('You win! The CPU choose', cpu_option , player_option)
+                win = True
+                result(win,cpu_option)
             elif cpu_option == 'paper':
-                print('You lose! The CPU choose', cpu_option)
+                win = False
+                result(win,cpu_option)
         elif player_option == 'paper':
             if cpu_option == 'rock':
-                print('You win! The CPU choose', cpu_option)
+                win = True
+                result(win,cpu_option)
             elif cpu_option == 'scissor':
-                print('You lose! The CPU choose', cpu_option)        
+                win = False
+                result(win,cpu_option)        
         elif player_option == 'scissor':
             if cpu_option == 'paper':
-                print('You win! The CPU choose', cpu_option)
+                win = True
+                result(win,cpu_option)
             elif cpu_option == 'rock':
-                print('You lose! The CPU choose', cpu_option)
+                win = False
+                result(win,cpu_option)
 
         stay = input('Wanna continue? (y/n) \n')
         if stay == 'y':
             game_on = True
+            player = 0
         elif stay == 'n':
             game_on = False
         else:
